852552daf38e5c634ed5ecb60c60756b10e171b0
deus-tll <millerartemka14@gmail.com>
2022-11-16 00:16:42 +0200

    update

--- Best_Oil/Prices/Form1.Designer.cs
+++ Best_Oil/Prices/Form1.Designer.cs
@@ -28,118 +28,312 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.panel2 = new System.Windows.Forms.Panel();
 			this.GridFuel = new System.Windows.Forms.DataGridView();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.panel2 = new System.Windows.Forms.Panel();
 			this.GridProduct = new System.Windows.Forms.DataGridView();
-			this.Col_Products = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Col_PriceProduct = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
 			this.Col_Fuel = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.Col_PriceFuel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Col_Products = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Col_PriceProduct = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
 			this.panel1.SuspendLayout();
-			this.panel2.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.GridFuel)).BeginInit();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.panel2.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.GridProduct)).BeginInit();
+			this.tableLayoutPanel3.SuspendLayout();
+			this.tableLayoutPanel4.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.IsSplitterFixed = true;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel2);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+			this.splitContainer1.Size = new System.Drawing.Size(782, 493);
+			this.splitContainer1.SplitterDistance = 389;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.BackColor = System.Drawing.Color.Indigo;
+			this.tableLayoutPanel2.ColumnCount = 1;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 1);
+			this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 2;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.05F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.95F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(389, 493);
+			this.tableLayoutPanel2.TabIndex = 0;
 			// 
 			// panel1
 			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.panel1.AutoScroll = true;
 			this.panel1.Controls.Add(this.GridFuel);
-			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Location = new System.Drawing.Point(3, 57);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(376, 469);
-			this.panel1.TabIndex = 0;
-			// 
-			// panel2
-			// 
-			this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.panel2.AutoScroll = true;
-			this.panel2.Controls.Add(this.GridProduct);
-			this.panel2.Location = new System.Drawing.Point(394, 12);
-			this.panel2.Name = "panel2";
-			this.panel2.Size = new System.Drawing.Size(376, 469);
-			this.panel2.TabIndex = 1;
+			this.panel1.Size = new System.Drawing.Size(383, 433);
+			this.panel1.TabIndex = 4;
 			// 
 			// GridFuel
 			// 
+			this.GridFuel.BackgroundColor = System.Drawing.Color.CadetBlue;
 			this.GridFuel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.GridFuel.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-			this.Col_Fuel,
-			this.Col_PriceFuel});
+            this.Col_Fuel,
+            this.Col_PriceFuel});
 			this.GridFuel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.GridFuel.GridColor = System.Drawing.Color.Black;
 			this.GridFuel.Location = new System.Drawing.Point(0, 0);
 			this.GridFuel.Name = "GridFuel";
-			this.GridFuel.Size = new System.Drawing.Size(376, 469);
+			this.GridFuel.Size = new System.Drawing.Size(383, 433);
 			this.GridFuel.TabIndex = 0;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.BackColor = System.Drawing.Color.Indigo;
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.05328F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.94672F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(389, 493);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.AutoScroll = true;
+			this.panel2.Controls.Add(this.GridProduct);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel2.Location = new System.Drawing.Point(3, 57);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(383, 433);
+			this.panel2.TabIndex = 5;
 			// 
 			// GridProduct
 			// 
+			this.GridProduct.BackgroundColor = System.Drawing.Color.CadetBlue;
 			this.GridProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.GridProduct.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-			this.Col_Products,
-			this.Col_PriceProduct});
+            this.Col_Products,
+            this.Col_PriceProduct});
 			this.GridProduct.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.GridProduct.GridColor = System.Drawing.Color.Black;
 			this.GridProduct.Location = new System.Drawing.Point(0, 0);
 			this.GridProduct.Name = "GridProduct";
-			this.GridProduct.Size = new System.Drawing.Size(376, 469);
+			this.GridProduct.Size = new System.Drawing.Size(383, 433);
 			this.GridProduct.TabIndex = 2;
 			// 
-			// Col_Products
+			// tableLayoutPanel3
 			// 
-			this.Col_Products.DataPropertyName = "ProductName";
-			this.Col_Products.HeaderText = "Products";
-			this.Col_Products.Name = "Col_Products";
-			this.Col_Products.Width = 166;
+			this.tableLayoutPanel3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.tableLayoutPanel3.ColumnCount = 3;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel3.Controls.Add(this.button1, 0, 0);
+			this.tableLayoutPanel3.Controls.Add(this.button2, 1, 0);
+			this.tableLayoutPanel3.Controls.Add(this.button3, 2, 0);
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(92, 3);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 1;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(205, 48);
+			this.tableLayoutPanel3.TabIndex = 5;
 			// 
-			// Col_PriceProduct
+			// tableLayoutPanel4
 			// 
-			this.Col_PriceProduct.DataPropertyName = "Price";
-			this.Col_PriceProduct.HeaderText = "Price";
-			this.Col_PriceProduct.Name = "Col_PriceProduct";
-			this.Col_PriceProduct.Width = 166;
+			this.tableLayoutPanel4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.tableLayoutPanel4.ColumnCount = 3;
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel4.Controls.Add(this.button4, 0, 0);
+			this.tableLayoutPanel4.Controls.Add(this.button5, 1, 0);
+			this.tableLayoutPanel4.Controls.Add(this.button6, 2, 0);
+			this.tableLayoutPanel4.Location = new System.Drawing.Point(92, 3);
+			this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+			this.tableLayoutPanel4.RowCount = 1;
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel4.Size = new System.Drawing.Size(205, 48);
+			this.tableLayoutPanel4.TabIndex = 6;
+			// 
+			// button1
+			// 
+			this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button1.Location = new System.Drawing.Point(3, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(62, 42);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button2.Location = new System.Drawing.Point(71, 3);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(62, 42);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button3.Location = new System.Drawing.Point(139, 3);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(63, 42);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button4.Location = new System.Drawing.Point(3, 3);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(62, 42);
+			this.button4.TabIndex = 0;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button5.Location = new System.Drawing.Point(71, 3);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(62, 42);
+			this.button5.TabIndex = 1;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button6.Location = new System.Drawing.Point(139, 3);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(63, 42);
+			this.button6.TabIndex = 2;
+			this.button6.Text = "button6";
+			this.button6.UseVisualStyleBackColor = true;
 			// 
 			// Col_Fuel
 			// 
+			this.Col_Fuel.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
 			this.Col_Fuel.DataPropertyName = "FuelName";
 			this.Col_Fuel.HeaderText = "Fuel";
 			this.Col_Fuel.Name = "Col_Fuel";
-			this.Col_Fuel.Width = 166;
+			this.Col_Fuel.ReadOnly = true;
 			// 
 			// Col_PriceFuel
 			// 
+			this.Col_PriceFuel.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
 			this.Col_PriceFuel.DataPropertyName = "Price";
 			this.Col_PriceFuel.HeaderText = "Price";
 			this.Col_PriceFuel.Name = "Col_PriceFuel";
-			this.Col_PriceFuel.Width = 166;
+			this.Col_PriceFuel.ReadOnly = true;
+			// 
+			// Col_Products
+			// 
+			this.Col_Products.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Col_Products.DataPropertyName = "ProductName";
+			this.Col_Products.HeaderText = "Products";
+			this.Col_Products.Name = "Col_Products";
+			this.Col_Products.ReadOnly = true;
+			// 
+			// Col_PriceProduct
+			// 
+			this.Col_PriceProduct.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Col_PriceProduct.DataPropertyName = "Price";
+			this.Col_PriceProduct.HeaderText = "Price";
+			this.Col_PriceProduct.Name = "Col_PriceProduct";
+			this.Col_PriceProduct.ReadOnly = true;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(782, 493);
-			this.Controls.Add(this.panel2);
-			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.splitContainer1);
+			this.MinimumSize = new System.Drawing.Size(798, 532);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.tableLayoutPanel2.ResumeLayout(false);
 			this.panel1.ResumeLayout(false);
-			this.panel2.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.GridFuel)).EndInit();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.GridProduct)).EndInit();
+			this.tableLayoutPanel3.ResumeLayout(false);
+			this.tableLayoutPanel4.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
+		private System.Windows.Forms.SplitContainer splitContainer1;
 		private System.Windows.Forms.Panel panel1;
 		private System.Windows.Forms.DataGridView GridFuel;
 		private System.Windows.Forms.Panel panel2;
 		private System.Windows.Forms.DataGridView GridProduct;
-		private System.Windows.Forms.DataGridViewTextBoxColumn Col_Products;
-		private System.Windows.Forms.DataGridViewTextBoxColumn Col_PriceProduct;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Col_Fuel;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Col_PriceFuel;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Col_Products;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Col_PriceProduct;
 	}
 }
