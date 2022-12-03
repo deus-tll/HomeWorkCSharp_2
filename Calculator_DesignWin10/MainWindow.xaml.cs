94afc77ae4c1fce490f21dbd39547e035497a827
deus-tll <millerartemka14@gmail.com>
2022-12-03 16:42:17 +0200

    update

---
+++ Calculator_DesignWin10/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_DesignWin10
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			RightPanel.Visibility = (Width > 577) ? Visibility.Visible : Visibility.Collapsed;
+
+			if (Width < 800 && Width > 577)
+			{
+				RightPanel.Width = (41.7677642980936 / 100) * Width;
+			}
+
+		}
+	}
+}
--- Money/Calculate.cs
+++ Money/Calculate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Money
+﻿namespace Money
 {
 	public class Calculate
 	{
@@ -18,6 +12,7 @@ namespace Money
 		private void AddF(int f)
 		{
 			int tmp = _f + f;
+
 			if (tmp == 100)
 			{
 				++_p;
