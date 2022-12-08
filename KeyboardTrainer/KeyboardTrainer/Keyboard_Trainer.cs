48200fe6f4fcac591e02422abac9719896edee6d
deus-tll <millerartemka14@gmail.com>
2022-12-08 18:22:49 +0200

    update

---
+++ KeyboardTrainer/KeyboardTrainer/Keyboard_Trainer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyboardTrainer
+{
+	class Keyboard_Trainer
+	{
+		private const string LOWER_CASE = "qwertyuiopasdfghjklzxcvbnm";
+		private const string UPPER_CASE = "QWERTYUIOPASDFGHJKLZXCVBNM";
+		private const string ADDITIONAL_SYMBOLS = "1234567890`-=[]\\;',./~!@#$%^&*()_+{}|:\"<>?";
+
+
+		public string SourceString { get; private set; }
+		public string SourceCorrectString { get; private set; }
+		public string InputCorrectString { get; private set; }
+
+		public int SpeedOfTyping { get; private set; }
+		public int CountOfFails { get; private set; }
+
+		private int _countOfCorrectChars;
+
+		public void UpdateString()
+		{
+			SourceCorrectString += SourceString[0];
+			InputCorrectString += SourceString[0];
+
+			SourceString = SourceString.Remove(0, 1);
+		}
+
+		public void UpdateCountOfCorrectChars() => ++_countOfCorrectChars;
+		public void UpdateCountOfFails() => ++CountOfFails;
+		public void UpdateSpeedOfTyping(int amountOfElapsedTime) => SpeedOfTyping = (_countOfCorrectChars * 60) / amountOfElapsedTime;
+
+		public void GenerateString(int difficulty, bool? upperLower_case, bool? symbols)
+		{
+			SourceCorrectString = "";
+			InputCorrectString = "";
+
+			StringBuilder builder = new StringBuilder();
+			Random random = new Random();
+
+
+			string s = "";
+			int ind;
+			for (int i = 0; i < difficulty; )
+			{
+				ind = random.Next(0, LOWER_CASE.Length);
+				if (s.IndexOf(LOWER_CASE[ind]) == -1)
+				{
+					s += LOWER_CASE[ind];
+					++i;
+				}
+			}
+
+			if ((bool)upperLower_case)
+			{
+				s += s.ToUpper();
+				Generate(ref builder, s);
+			}
+			else
+			{
+				Generate(ref builder, s);
+			}
+
+			if ((bool)symbols)
+			{
+				int tmp;
+				int countOfSymbols = random.Next(5, 15);
+				List<int> alreadyHas = new List<int>();
+
+				for (int i = 0; i < countOfSymbols; )
+				{
+					tmp = random.Next(0, builder.Length);
+
+					if (alreadyHas.FindIndex(f => f == tmp) == -1)
+					{
+						alreadyHas.Add(tmp);
+						builder.Remove(tmp, 1);
+						builder.Insert(tmp, ADDITIONAL_SYMBOLS[random.Next(0, ADDITIONAL_SYMBOLS.Length)]);
+
+						i++;
+					}
+
+				}
+			}
+
+			SourceString = builder.ToString();
+		}
+		private void Generate(ref StringBuilder builder, string s)
+		{
+			int wordLength;
+			Random random = new Random();
+
+
+			for (int i = 0; i < 67; i++)
+			{
+				wordLength = random.Next(1, 10);
+
+				if (i + wordLength < 67)
+				{
+					i += wordLength;
+				}
+				else
+				{
+					i += wordLength;
+					while (i != 67)
+					{
+						--wordLength;
+						--i;
+					}
+				}
+
+				for (int j = 0; j < wordLength; j++)
+					builder.Append(s[random.Next(0, s.Length)]);
+
+				if (i<67)
+					builder.Append(' ');
+			}
+		}
+
+		public void Reset()
+		{
+			SourceString = "";
+			SourceCorrectString = "";
+			InputCorrectString = "";
+			SpeedOfTyping = 0;
+			CountOfFails = 0;
+			_countOfCorrectChars = 0;
+		}
+	}
+}
---
+++ KeyboardTrainer/KeyboardTrainer/MainWindow.xaml.cs
@@ -0,0 +1,368 @@
+﻿using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Documents;
+using System.Reflection;
+using System.Windows.Data;
+
+namespace KeyboardTrainer
+{
+	internal sealed partial class MainWindow : Window
+	{
+		private Keyboard_Trainer _keyboardTrainer = new Keyboard_Trainer();
+		private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+		private int _amountOfElapsedTime;
+		private bool inProcess = false;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			dispatcherTimer.Tick += DispatcherTimer_Tick;
+			dispatcherTimer.Interval = new System.TimeSpan(0,0,1);
+		}
+
+		private void DispatcherTimer_Tick(object sender, System.EventArgs e)
+		{
+			++_amountOfElapsedTime;
+			_keyboardTrainer.UpdateSpeedOfTyping(_amountOfElapsedTime);
+			txtBlock_Speed.Text = _keyboardTrainer.SpeedOfTyping.ToString();
+		}
+
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			string charEntered = null;
+
+			IsShift_AdditionalSymbol(ref charEntered, e);
+
+			if (e.Key == Key.Enter || 
+				e.Key == Key.Back || 
+				e.Key == Key.F1 || 
+				e.Key == Key.F2 || 
+				e.Key == Key.F3 || 
+				e.Key == Key.F4 || 
+				e.Key == Key.F5 ||
+				e.Key == Key.F6 ||
+				e.Key == Key.F7 ||
+				e.Key == Key.F7 ||
+				e.Key == Key.F8 ||
+				e.Key == Key.F9 ||
+				e.Key == Key.F10 ||
+				e.Key == Key.F11 ||
+				e.Key == Key.F12 ||
+				e.Key == Key.Escape ||
+				e.Key == Key.PageDown ||
+				e.Key == Key.PageUp ||
+				e.Key == Key.Home ||
+				e.Key == Key.End ||
+				e.Key == Key.Delete ||
+				e.Key == Key.Up ||
+				e.Key == Key.Left ||
+				e.Key == Key.Right ||
+				e.Key == Key.Down ||
+				e.Key == Key.LeftAlt ||
+				e.Key == Key.RightAlt ||
+				e.Key == Key.LeftCtrl ||
+				e.Key == Key.RightCtrl ||
+				e.Key == Key.Tab ||
+				e.Key == Key.Scroll ||
+				e.Key == Key.PrintScreen ||
+				e.Key == Key.Insert ||
+				e.Key == Key.LWin ||
+				e.Key == Key.RWin ||
+				e.Key == Key.Apps ||
+				e.Key == Key.System ||
+				e.Key == Key.Pause)
+			{
+				return;
+			}
+
+
+			if (e.Key == Key.CapsLock)
+				return;
+
+
+			if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
+			{
+				UpperCase();
+
+				if (e.KeyboardDevice.GetKeyStates(Key.CapsLock) == KeyStates.Toggled)
+					LowerCase();
+				else if (e.KeyboardDevice.GetKeyStates(Key.CapsLock) == KeyStates.None)
+					UpperCase();
+
+				return;
+			}
+			else
+			{
+				if (_keyboardTrainer.SourceString == null || !inProcess)
+					return;
+
+				if (charEntered != null)
+				{
+					SymbolIsRight(Convert.ToChar(charEntered));
+				}
+				else
+				{
+					charEntered = e.Key.ToString();
+					IsCapsAndShift_Letter(charEntered, e);
+				}
+			}	
+		}
+
+
+		private void IsShift_AdditionalSymbol(ref string charEntered, KeyEventArgs e)
+		{
+			if ((e.KeyboardDevice.Modifiers & ModifierKeys.Shift) != ModifierKeys.Shift)
+			{
+				switch (e.Key)
+				{
+					case Key.Space:
+						charEntered = " ";
+						break;
+					case Key.D0:
+						charEntered = "0";
+						break;
+					case Key.D1:
+						charEntered = "1";
+						break;
+					case Key.D2:
+						charEntered = "2";
+						break;
+					case Key.D3:
+						charEntered = "3";
+						break;
+					case Key.D4:
+						charEntered = "4";
+						break;
+					case Key.D5:
+						charEntered = "5";
+						break;
+					case Key.D6:
+						charEntered = "6";
+						break;
+					case Key.D7:
+						charEntered = "7";
+						break;
+					case Key.D8:
+						charEntered = "8";
+						break;
+					case Key.D9:
+						charEntered = "9";
+						break;
+					case Key.Oem3:
+						charEntered = "`";
+						break;
+					case Key.OemMinus:
+						charEntered = "-";
+						break;
+					case Key.OemPlus:
+						charEntered = "=";
+						break;
+					case Key.OemOpenBrackets:
+						charEntered = "[";
+						break;
+					case Key.Oem6:
+						charEntered = "]";
+						break;
+					case Key.Oem5:
+						charEntered = "\\";
+						break;
+					case Key.Oem1:
+						charEntered = ";";
+						break;
+					case Key.OemQuotes:
+						charEntered = "'";
+						break;
+					case Key.OemComma:
+						charEntered = ",";
+						break;
+					case Key.OemPeriod:
+						charEntered = ".";
+						break;
+					case Key.OemQuestion:
+						charEntered = "/";
+						break;
+				}
+			}
+			else
+			{
+				switch (e.Key)
+				{
+					case Key.Space:
+						charEntered = " ";
+						break;
+					case Key.D0:
+						charEntered = ")";
+						break;
+					case Key.D1:
+						charEntered = "!";
+						break;
+					case Key.D2:
+						charEntered = "@";
+						break;
+					case Key.D3:
+						charEntered = "#";
+						break;
+					case Key.D4:
+						charEntered = "$";
+						break;
+					case Key.D5:
+						charEntered = "%";
+						break;
+					case Key.D6:
+						charEntered = "^";
+						break;
+					case Key.D7:
+						charEntered = "&";
+						break;
+					case Key.D8:
+						charEntered = "*";
+						break;
+					case Key.D9:
+						charEntered = "(";
+						break;
+					case Key.Oem3:
+						charEntered = "~";
+						break;
+					case Key.OemMinus:
+						charEntered = "_";
+						break;
+					case Key.OemPlus:
+						charEntered = "+";
+						break;
+					case Key.OemOpenBrackets:
+						charEntered = "{";
+						break;
+					case Key.Oem6:
+						charEntered = "}";
+						break;
+					case Key.Oem5:
+						charEntered = "|";
+						break;
+					case Key.Oem1:
+						charEntered = ":";
+						break;
+					case Key.OemQuotes:
+						charEntered = "\"";
+						break;
+					case Key.OemComma:
+						charEntered = "<";
+						break;
+					case Key.OemPeriod:
+						charEntered = ">";
+						break;
+					case Key.OemQuestion:
+						charEntered = "?";
+						break;
+				}
+			}
+		}
+
+
+		private void IsCapsAndShift_Letter(string charEntered, KeyEventArgs e)
+		{
+			KeyStates capsLock = e.KeyboardDevice.GetKeyStates(Key.CapsLock);
+
+			if (capsLock != KeyStates.Toggled)
+			{
+				if ((e.KeyboardDevice.Modifiers & ModifierKeys.Shift) != ModifierKeys.Shift)
+					charEntered = charEntered.ToLower();
+			}
+			else
+			{
+				if ((e.KeyboardDevice.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift)
+					charEntered = charEntered.ToLower();
+			}
+
+			SymbolIsRight(Convert.ToChar(charEntered));
+		}
+
+
+		private void SymbolIsRight(char ch)
+		{
+			if (ch == _keyboardTrainer.SourceString[0])
+			{
+				_keyboardTrainer.UpdateString();
+				_keyboardTrainer.UpdateCountOfCorrectChars();
+
+				if (_keyboardTrainer.SourceString.Length == 0)
+					_keyboardTrainer.GenerateString(Convert.ToInt32(slider_Difficulty.Value), chekBox_CaseSens.IsChecked, chekBox_AdditionalSymbols.IsChecked);
+
+
+				RefreshString();
+			}
+			else
+			{
+				_keyboardTrainer.UpdateCountOfFails();
+				txtBlock_Fails.Text = _keyboardTrainer.CountOfFails.ToString();
+			}
+		}
+
+
+		private void Window_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
+				LowerCase();
+
+			if (e.KeyboardDevice.GetKeyStates(Key.CapsLock) == KeyStates.Toggled)
+				UpperCase();
+			else if (e.KeyboardDevice.GetKeyStates(Key.CapsLock) == KeyStates.None)
+				LowerCase();
+		}
+
+
+		private void RefreshString()
+		{
+			txtBlock_SourceText.Text = _keyboardTrainer.SourceString;
+			txtBlock_CorrectSourceText.Text = _keyboardTrainer.SourceCorrectString;
+			txtBlock_CorrectInputText.Text = _keyboardTrainer.InputCorrectString;
+		}
+
+
+		private void UpperCase()
+		{
+			stackPanel_UpperCaseAndAdditionalSymbols.Visibility = Visibility.Visible;
+			stackPanel_LowerCaseAndAdditionalSymbols.Visibility = Visibility.Collapsed;
+		}
+		private void LowerCase()
+		{
+			stackPanel_UpperCaseAndAdditionalSymbols.Visibility = Visibility.Collapsed;
+			stackPanel_LowerCaseAndAdditionalSymbols.Visibility = Visibility.Visible;
+		}
+
+		private void btn_Start_Click(object sender, RoutedEventArgs e)
+		{
+			_keyboardTrainer.Reset();
+			txtBlock_Fails.Text = "0";
+			txtBlock_Speed.Text = "0";
+
+			_keyboardTrainer.GenerateString(Convert.ToInt32(slider_Difficulty.Value), chekBox_CaseSens.IsChecked, chekBox_AdditionalSymbols.IsChecked);
+			RefreshString();
+
+			dispatcherTimer.Start();
+
+			btn_Stop.IsEnabled = true;
+			btn_Start.IsEnabled = false;
+			inProcess = true;
+		}
+
+		private void btn_Stop_Click(object sender, RoutedEventArgs e)
+		{
+			dispatcherTimer.Stop();
+			btn_Stop.IsEnabled = false;
+			btn_Start.IsEnabled = true;
+			inProcess = false;
+		}
+
+		private void slider_Difficulty_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			int i = Convert.ToInt32(slider_Difficulty.Value);
+			txtBlock_Difficulty.Text = i.ToString();
+		}
+    }
+}
